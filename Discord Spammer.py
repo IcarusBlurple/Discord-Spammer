795ef7a60bfd06775cd974df92c088b8b46801ab
IcarusBlurple <71614821+IcarusBlurple@users.noreply.github.com>
2020-11-06 09:51:54 -0600

    Add files via upload

---
+++ Discord Spammer.py
@@ -0,0 +1,28 @@
+'''
+Made by Icarus
+Discord Icarus#6179
+'''
+import time
+import keyboard
+
+
+
+def TextSpammer():
+    print('''
+            Discord Text Spammer
+          ''')
+    user_msg = input("What Do You Want To Spam ? :")
+    spam_amount = int(input("How Many Times Do You Want To Spam ? :"))
+    counter = 0
+    spam_msg = '\n' + user_msg
+    print("Wait 5 Sec Before The Spam Starts ....")
+    time.sleep(5)
+    while True:
+        if counter >= spam_amount:
+            break
+        else:
+            keyboard.write(spam_msg)
+            counter = counter + 1
+
+
+TextSpammer()
