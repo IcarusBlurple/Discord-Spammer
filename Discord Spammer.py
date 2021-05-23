f38c56cefeb02b21dc1f151c517f725f8b57276a
robi0t <68228472+robi0t@users.noreply.github.com>
2021-05-23 15:38:10 +0300

    Fixed small stuff
    Fixed handling for if the user doesen't input a valid number. 
    Changed "counter = counter + 1" to "counter += 1" . It does the exact same thing but it's simpler
    Added ascii art
    
    Keep on with python, and good luck for future projects :))

--- Discord Spammer.py
+++ Discord Spammer.py
@@ -5,24 +5,38 @@ Discord Maybe#1337
 import time
 import keyboard
 
-
+banner='''
+  _____  _                       _   _______        _      _____                                           
+ |  __ \(_)                     | | |__   __|      | |    / ____|                                          
+ | |  | |_ ___  ___ ___  _ __ __| |    | | _____  _| |_  | (___  _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
+ | |  | | / __|/ __/ _ \| '__/ _` |    | |/ _ \ \/ / __|  \___ \| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+ | |__| | \__ \ (_| (_) | | | (_| |    | |  __/>  <| |_   ____) | |_) | (_| | | | | | | | | | | |  __/ |   
+ |_____/|_|___/\___\___/|_|  \__,_|    |_|\___/_/\_\\__| |_____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
+                                                                | |                                        
+                                                                |_|
+'''
 
 def TextSpammer():
-    print('''
-            Discord Text Spammer
-          ''')
+    print(banner)
     user_msg = input("What Do You Want To Spam ? :")
-    spam_amount = int(input("How Many Times Do You Want To Spam ? :"))
+    try:
+        spam_amount = int(input("How Many Times Do You Want To Spam ? :"))
+        wait_time = int(input("How many seconds do you want to wait until the spamming starts ? : "))
+    except:
+        print("The value given is not a valid number")
+        return
+    
+    
     counter = 0
     spam_msg = '\n' + user_msg
     print("Wait 5 Sec Before The Spam Starts ....")
-    time.sleep(5)
+    time.sleep(wait_time)
     while True:
         if counter >= spam_amount:
             break
         else:
             keyboard.write(spam_msg)
-            counter = counter + 1
+            counter += 1
 
 
 TextSpammer()
